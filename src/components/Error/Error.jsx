93388fc4a209366b9bf4507636a02a4ee1d5dd83
NissanJK <jawadul.karim78@gmail.com>
2024-11-03 22:23:29 +0600

    initial setup done

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+const Error = () => {
+    return (
+        <div>
+            <div className="text-center font-black text-4xl">
+                <h1 className="">Oops!</h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root';
+import Error from './components/Error/Error';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
